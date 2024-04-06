0fe6f45aded7146a5ab6cac998e7e678305410a7
Emmanuel-Deo <emmanauel.kipngetich.ngeno@gmail.com>
2024-04-06 22:26:30 +0300

    Add New Text

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Emmanuel!</Text>
+      <Text>Emmanuel !</Text>
+      <Text>Linda!</Text>
       <StatusBar style="auto" />
     </View>
   );
