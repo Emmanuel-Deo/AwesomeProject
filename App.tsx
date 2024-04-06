81437b88db1a9ca6fd9a030df03ed9db3a326dd5
Emmanuel-Deo <emmanauel.kipngetich.ngeno@gmail.com>
2024-04-07 01:04:52 +0300

    Advent Calender List

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import DayListItems from './src/components/core/DayListItems';
+
+const days = [...Array(25)].map((val, index)=>index+1);
+console.log(days)
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Emmanuel !</Text>
-      <Text>Linda!</Text>
+
+      <FlatList
+      contentContainerStyle = {styles.content}
+      columnWrapperStyle = {styles.column}
+      numColumns={2}
+        data = {days}
+        renderItem={({item})=><DayListItems day = {item} />}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +26,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  content:{
+    gap: 10,
+    padding : 10,
+  },
+  column:{
+    gap: 10,
   },
 });
