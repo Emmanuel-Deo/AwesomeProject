c6784352fc2135ee1f8d62fdacd5cd745b819a07
Emmanuel-Deo <emmanauel.kipngetich.ngeno@gmail.com>
2024-04-07 15:22:45 +0300

    Custom Google Fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 import DayListItems from './src/components/core/DayListItems';
+
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
+
+
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(25)].map((val, index)=>index+1);
 console.log(days)
 
 export default function App() {
+  const[fontsLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic : AmaticSC_400Regular,
+    AmaticBold : AmaticSC_700Bold,
+  });
+
+  useEffect(()=>{
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+   }
+  },[fontsLoaded,fontError]);
+
+  if (!fontsLoaded && !fontError){
+    return null;
+  }
+
+ 
   return (
     <View style={styles.container}>
 
